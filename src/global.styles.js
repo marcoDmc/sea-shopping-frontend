822b9096435eef1ac38fc3b36edfba3352c0f70b
marcosDmc <marcodamasceno07@gmail.com>
2021-11-29 23:15:55 -0300

    arquivos deletados

---
+++ src/global.styles.js
@@ -0,0 +1,14 @@
+import {createGlobalStyle} from "styled-components";
+
+
+export const Global = createGlobalStyle`
+*{
+    padding: 0px;
+    margin:0px;
+    box-sizing: border-box;
+}
+
+body{
+    background: #F8FSEC;   
+}
+`
